4c0fa22583d51fc2219f15cdd2afaacc87b9c5f9
kristian:) <68247804+kristian-chaves@users.noreply.github.com>
2025-04-22 01:37:44 -0400

    Update aristocrats_teysa.py

---
+++ aristocrats_teysa.py
@@ -0,0 +1,20 @@
+
+
+
+#outline functions for use
+
+# create a card type - each card has a 
+    #   mana cost
+    # associated function (effect)
+
+# this decks play pattern involves acruing spirits - each creature has nine lives
+# each turn it checks for valid resources, 
+
+def teysaDraw():
+    print()
+
+def teysaSac():
+    print()
+
+
+deck = []
